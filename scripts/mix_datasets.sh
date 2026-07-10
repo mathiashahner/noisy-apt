74ec676843658196848466a636ec30a40f232114
Mathias Hahner <mathias_hahner@hotmail.com>
2026-07-09 22:07:39 -0300

    Fix mix_datasets script

--- scripts/mix_datasets.sh
+++ scripts/mix_datasets.sh
@@ -1,4 +1,4 @@
-uv run ./scripts/build_maestro_musdb_mixtures.py \
+python ./scripts/build_maestro_musdb_mixtures.py \
   --maestro_root ./data/maestro-v3.0.0 \
   --musdb_root ./data/musdb18hq \
   --out_root ./data/maestro_musdb_mixed \
